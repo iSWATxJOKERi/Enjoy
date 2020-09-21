340d0451b0b680034fd374ae6753e55a2f1b1fe2
swatjoker <iSWATxJOKERi>
2020-09-21 19:29:53 -0400

    first  branch

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import * as APIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
-export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_ERRORS";
 
 const receiveCurrentUser = currentUser => {
     return {
@@ -11,9 +11,9 @@ const receiveCurrentUser = currentUser => {
     }
 }
 
-const receiveErrors = errors => {
+const receiveSessionErrors = errors => {
     return {
-        type: RECEIVE_ERRORS,
+        type: RECEIVE_SESSION_ERRORS,
         errors
     }
 }
@@ -28,7 +28,7 @@ export const login = user = dispatch => {
     return APIUtil.login(user).then(user => {
         dispatch(receiveCurrentUser(user))
     }, errors => {
-        dispatch(receiveErrors(errors))
+        dispatch(receiveSessionErrors(errors))
     })
 }
 
@@ -36,7 +36,7 @@ export const signup = user = dispatch => {
     return APIUtil.signup(user).then(user => {
         dispatch(receiveCurrentUser(user))
     }, errors => {
-        dispatch(receiveErrors(errors))
+        dispatch(receiveSessionErrors(errors))
     })
 }
 
--- frontend/enjoy.jsx
+++ frontend/enjoy.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
+    const root = document.getElementById('root');
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     window.signup = signup;
     window.login = login;
     window.logout = logout;
-    const root = document.getElementById('root');
+
     ReactDOM.render(<h1>Enjoy</h1>, root)
 })
