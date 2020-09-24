2e8ecab952daaf2fc5eff34e2021a75dc3e123ec
swatjoker <iSWATxJOKERi>
2020-09-24 09:13:48 -0400

    signup

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../../font_awesome';
 
 class SessionForm extends React.Component {
     constructor(props) {
@@ -19,7 +21,7 @@ class SessionForm extends React.Component {
     handleSubmit(e) {
         let form = this.props.formType === 'Sign in' ? 
             { email: this.state['email'], password: this.state['password'] } :
-             { email: this.state['email'], password: this.state['password'], email: this.state['email']}
+             { email: this.state['email'], password: this.state['password'], username: this.state['username']}
         e.preventDefault();
         const user = Object.assign({}, form);
         this.props.processForm(user).then(() => this.props.history.push("/"));
@@ -42,7 +44,9 @@ class SessionForm extends React.Component {
 
     render() {
         let form;
+        let form2;
         let arr;
+        const user = <FontAwesomeIcon icon="user-circle" />
         let errors = this.props.errors;
         if(this.props.formType === 'Sign in') {
             form = <div className="session-form">
@@ -57,11 +61,16 @@ class SessionForm extends React.Component {
                 </div>
             </div>
         }else {
-            form = <div className="session-form">
-                <label className="username">Username<input className="username-field" type="text" value={ this.state.username } onChange={ this.handleInput('username') } /></label>
-                <label className="password">Password<input className="password-field" type="password" value={ this.state.password } onChange={ this.handleInput('password') } /></label>
-                <label className="email">Email<input className="email-field" type="email" value={ this.state.email } onChange={ this.handleInput('email') } /></label>
-                <button className="submit-session" type="submit">Submit</button>
+            form2 = <div className="session-form2">
+                <div className="formleft">
+                    <input className="username-field2" type="text" placeholder="Username" value={ this.state.username } onChange={ this.handleInput('username') } />
+                    <input className="password-field2" type="password" placeholder="Password" value={ this.state.password } onChange={ this.handleInput('password') } />
+                    <input className="email-field2" type="email" value={ this.state.email } placeholder="Email" onChange={ this.handleInput('email') } />
+                    <button className="submit-session2" type="submit">Submit</button>
+                </div>
+                <div className="formright">
+                    { user }
+                </div>
             </div>
         }
         if(errors.length > 0) {
@@ -71,10 +80,10 @@ class SessionForm extends React.Component {
         }
         return (
             <section className="container">
-                <div className="form">
-                    <form onSubmit={ this.handleSubmit }>
-                        <h1 className="form-type">{ this.props.formType }</h1>
-                        { form }
+                <div className={ this.props.formType === 'Sign in' ? "form" : "otherform" }>
+                    <form className={ this.props.formType === 'Sign in' ? "form1" : "form2" } onSubmit={ this.handleSubmit }>
+                        <h1 className={ this.props.formType === 'Sign in' ? "form-type" : "form-type2"}>{ this.props.formType }</h1>
+                        { this.props.formType === 'Sign in' ? form : form2}
                     </form>
                     <ul className="errors">
                         { arr }
