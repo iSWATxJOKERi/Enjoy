941524c17ba822ca9ac40baca58d475ebc7aa008
swatjoker <iSWATxJOKERi>
2020-09-29 23:04:14 -0400

    sigh

--- frontend/components/user/edit_vids_item.jsx
+++ frontend/components/user/edit_vids_item.jsx
@@ -2,22 +2,55 @@ import React from 'react';
 import dateConverter from '../../util/date_converter';
 import VideoUpdateForm from './video_update_form';
 
-export default (props) => {
-    return (
-        <section className="edit-item">
-            <img id="user-video-images" onClick={ () => props.allProps.history.push(`/videos/${ props.vid.id }`) } src={ props.vid.photoUrl } />
-            <h1>{ props.vid.title }</h1>
-            <div className="-uploaded-box-date">
-                <span id="deets">6M views &#8226;</span>
-                <span id="deets"> { dateConverter(props.vid.created_at) }</span>
-            </div>
-            {/* { props.allProps.currentUser ?  */}
-            <div className="edit-delete-hover">
+export default class EditVids extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggleVideos = this.toggleVideos.bind(this);
+    }
+
+    componentDidMount() {
+        this.toggleVideos();
+    }
+    
+    toggleVideos() {
+        const videos = document.getElementsByClassName("abs-pos-modal")[0];
+        // const button = document.getElementsByClassName("edit-video");
+        // debugger
+        const close = document.getElementsByClassName("close3")[0];
+
+        document.querySelectorAll('.edit-video').forEach(item => {
+            item.addEventListener('click', e => {
+                debugger
+                videos.style.display = "block";
+            })
+        }) 
+
+        close.onclick = function() {
+            videos.style.display = "none";
+        }
+
+    }
+
+    render() {
+        let update;
+        if(this.props.allProps.currentUser && (this.props.vid.uploader.id === this.props.allProps.currentUser)) {
+            // debugger
+            update = <div className="edit-delete-hover">
                 <button className="edit-video">EDIT</button>
                 <button className="delete-video">DELETE</button>
-                <VideoUpdateForm allProps={ props.allProps } video={ props.vid }/>
-            </div> 
-            {/* : null } */}
-        </section>
-    )
+                <VideoUpdateForm allProps={ this.props.allProps } video={ this.props.vid }/>
+            </div>
+        }
+        return (
+            <section className="edit-item">
+                <img id="user-video-images" onClick={ () => this.props.allProps.history.push(`/videos/${ this.props.vid.id }`) } src={ this.props.vid.photoUrl } />
+                <h1>{ this.props.vid.title }</h1>
+                <div className="-uploaded-box-date">
+                    <span id="deets">6M views &#8226;</span>
+                    <span id="deets"> { dateConverter(this.props.vid.created_at) }</span>
+                </div>
+                { update }
+            </section>
+        )
+    }
 }
--- frontend/components/user/video_update_form.jsx
+++ frontend/components/user/video_update_form.jsx
@@ -6,17 +6,18 @@ class VideoUpdateForm extends React.Component {
         this.state = {
             title: "",
             description: "",
-            photoFile: null
+            photoFile: null,
+            photoUrl: null
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleFile = this.handleFile.bind(this);
         this.handleInput = this.handleInput.bind(this);
-        this.toggleVideos = this.toggleVideos.bind(this);
+        // this.toggleVideos = this.toggleVideos.bind(this);
     }
 
-    componentDidMount() {
-        this.toggleVideos();
-    }
+    // componentDidMount() {
+    //     this.toggleVideos();
+    // }
 
     handleInput(field) {
         return (e) => {
@@ -29,26 +30,34 @@ class VideoUpdateForm extends React.Component {
 
     handleFile(field) {
         return (e) => {
-            this.setState({
-                [field]: e.currentTarget.files[0]
-            })
+            const file = e.currentTarget.files[0];
+            const fileReader = new FileReader();
+            fileReader.onloadend = () => {
+                this.setState({
+                    [field]: file,
+                    photoUrl: fileReader.result
+                })
+            }
+            if(file) {
+                fileReader.readAsDataURL(file);
+            }
         }
     }
 
-    toggleVideos() {
-        const videos = document.getElementsByClassName("abs-pos-modal")[0];
-        const button = document.getElementsByClassName("edit-video")[0];
-        const close = document.getElementsByClassName("close3")[0];
+    // toggleVideos() {
+    //     const videos = document.getElementsByClassName("abs-pos-modal")[0];
+    //     const button = document.getElementsByClassName("edit-video")[0];
+    //     const close = document.getElementsByClassName("close3")[0];
 
-        button.onclick = function() {
-            videos.style.display = "block";
-        }
+    //     button.onclick = function() {
+    //         videos.style.display = "block";
+    //     }
 
-        close.onclick = function() {
-            videos.style.display = "none";
-        }
+    //     close.onclick = function() {
+    //         videos.style.display = "none";
+    //     }
 
-    }
+    // }
 
     handleSubmit(e) {
         e.preventDefault();
@@ -60,15 +69,25 @@ class VideoUpdateForm extends React.Component {
     }
 
     render() {
+        const preview1 = this.state.photoUrl ? <img className="thumbnail-preview" src={ this.state.photoUrl } /> : null;
         return (
             <section className="abs-pos-modal">
                 <section className="update-modal">
                     <form onSubmit={ this.handleSubmit } className="update-section">
                         <span className="close3">&times;</span>
-                        <label>Change Title<input type="text" id="form-title" value={ this.state.title } placeholder={ this.props.video.title } onChange={ this.handleInput('title') } /></label>
-                        <label>Change Description<textarea value={ this.state.description } placeholder={ this.props.video.description } id="form-description"  onChange={ this.handleInput('description')} /></label>
-                        <label>Change Thumbnail<input type="file" onChange={ this.handleFile('photoFile') }/></label>
-                        <button type="submit-update">Update</button>
+                        <div className="left-update">
+                            <div className="update-inputs">
+                                <input type="text" id="u-title" placeholder={ this.props.video.title } value={ this.state.title } onChange={ this.handleInput('title') } />
+                                <textarea value={ this.state.description } id="u-description" placeholder={ this.props.video.description } onChange={ this.handleInput('description')} />
+                            </div>
+                        </div>
+                        <div className="right-update">
+                            <div className="update-buttons">
+                                { preview1 }
+                                <label>Upload Thumbnail<input type="file" onChange={ this.handleFile('photoFile') }/></label>
+                            </div>
+                            <button className="submit-the-video2" type="submit">Upload</button>
+                        </div>
                     </form>
                 </section>
             </section>
