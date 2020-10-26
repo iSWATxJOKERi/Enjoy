a8d740f9a7fc7889b59c6a1d6fd0f74245bef0d1
swatjoker <iSWATxJOKERi>
2020-10-26 11:09:35 -0400

    starting to fix the project

--- frontend/components/main/main_content.jsx
+++ frontend/components/main/main_content.jsx
@@ -57,23 +57,6 @@ class MainContent extends React.Component {
                 { popout }
                 <SideBar allProps={ this.props }/>
                 <section className="index-section">
-                    <div className="list-of-tags">
-                        <li>All</li>
-                        <li>Rapping</li>
-                        <li>Lo-fi music</li>
-                        <li>Indie Music</li>
-                        <li>Electronic Music</li>
-                        <li>Thrillers</li>
-                        <li>Soul Music</li>
-                        <li>Comedy</li>
-                        <li>Transfer</li>
-                        <li>JavaScript</li>
-                        <li>Championships</li>
-                        <li>Trailers</li>
-                        <li>Football</li>
-                        <li>Xbox</li>
-                        <li>Halo</li>
-                    </div>
                     <section className="video-section">
                         <VideoIndex allProps={ this.props } videos={ this.props.videos } />
                     </section>
